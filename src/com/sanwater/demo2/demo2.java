8379e3c944ba07c1c5791582a2cb28963ccb38b8
魏青 <10750945454@qq.com>
2020-06-13 18:25:00 +0800

    加了一个东西

--- src/com/sanwater/demo2/demo2.java
+++ src/com/sanwater/demo2/demo2.java
@@ -4,8 +4,10 @@ import java.io.*;
 
 public class demo2 {
     public static void main(String[] args) {
+        File f = new File("D:\\a.txt");
         try {
-            BufferedReader r = new BufferedReader(new FileReader(new File("D:\\a.txt")));
+            FileReader in = new FileReader(f);
+            BufferedReader r = new BufferedReader(in);
             System.out.println(r.readLine());
             r.close();
             in.close();
